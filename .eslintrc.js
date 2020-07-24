2d8fe275bb603f10068316e15d96ebab8b35365e
Sebastian Perez <sebperez@gmail.com>
2020-07-23 22:15:45 -0400

    Carga inicial de configuración

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2020": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
