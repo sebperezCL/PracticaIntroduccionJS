3e404a73310584b39014a0115de02880233a2731
Sebastian Perez <sebperez@gmail.com>
2020-07-24 20:05:02 -0400

    Conversión de números decimales a romanos lista

--- prueba.js
+++ prueba.js
@@ -2,17 +2,26 @@
 "use strict"
 
 function covierteNumero(valor){
-    //console.log(`El valor ${valor} es número`)
     if(valor < 1 || valor > 3999){
         return 'Valor fuera de rango'
     }
-    let divisor = 1000
-    do {
-        console.log(parseInt(valor/divisor)) //(valor - valor%divisor)/divisor)
-        valor = valor%divisor
-        divisor /= 10        
-    } while (valor%divisor > 1)
-    console.log(valor%divisor)
+    let resultado = []
+    let sValor = valor.toString()
+    console.log(sValor)
+    
+    /*Array en dos dimensiones con las equivalencias entre cifras arábicas y números romanos
+    Nota: No encontré una solución más elegante :S */
+    let tabla = [['','M','MM','MMM'],['','C','CC','CCC','CD','D','DC','DCC','DCCC','CM'],['','X','XX','XXX','XL','L','LX','LXX','LXXX','XC'],
+    ['','I','II','III','IV','V','VI','VII','VIII','IX']]
+
+    //Con este ciclo recorremos el número entregado cifra por cifra, buscando en el array el equivalente en romano
+    for(let i=0, largo=sValor.length; i < largo; i++){
+        console.log(sValor.charAt(i))
+        //Nota: Esto sólo funcionará para el rango indicado en el ejercicio (número de 4 cifras)
+        resultado[i] = tabla[4-(largo-i)][parseInt(sValor.charAt(i))]
+    }
+    console.log(resultado)
+    return resultado
 }
 
 function convierteRomano(valor){
