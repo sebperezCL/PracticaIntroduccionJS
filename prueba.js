e33272f50c0cb092592cdf45f161ca025ad4370a
Sebastian Perez <sebperez@gmail.com>
2020-07-25 22:30:39 -0400

    Todas las validaciones ya terminadas para la conversión de decimal a romano

--- prueba.js
+++ prueba.js
@@ -3,7 +3,7 @@
 
 function covierteNumero(valor){
     if(valor < 1 || valor > 3999){
-        return 'Valor fuera de rango'
+        return 'Número fuera de rango'
     }
     let resultado = []
     let sValor = valor.toString()
@@ -23,35 +23,63 @@ function covierteNumero(valor){
 
 function convierteRomano(valor){
     let resultado = 0
-    let pos, posMas1, posMas2, posMas3
+    /*Esta variable: numeroMenor se usa para controlar que no aparezcan valores como D, C o M al final de la cadena, 
+    en general algún numero mayor a los ya evaluados. Permite filtrar valores como MIXX o XCD*/
+    let numeroMenor = 1000
+    let pos, posMas1
     for(let i=0, largo=valor.length; i < largo; i++){       
         pos = validarRomano(valor[i])        
         if(pos){
+            //
+            if(pos.numero > numeroMenor) {return 'No es romano válido - numero menor'}
             if(i+1 >= largo){
+                numeroMenor = pos.numero
                 resultado += pos.numero
             }else{
-                posMas1 = validarRomano(valor[i+1])
+                posMas1 = validarRomano(valor[i+1])                
                 if(pos.romano == 'V' || pos.romano == 'L' || pos.romano == 'D'){
+                    if(valor.split(valor[i]).length > 2){
+                        return 'No es romano - con split'
+                    }
                     if(pos.numero <= posMas1.numero){
                         return 'No es romano'                        
-                    }else{ resultado += pos.numero }
+                    }else{ 
+                        numeroMenor = pos.numero
+                        resultado += pos.numero 
+                    }
                 }else{
+                    if(valor.split(valor[i]).length > 4){
+                        return 'No es romano - con split'
+                    }
                     if(pos.numero >= posMas1.numero){
+                        numeroMenor = pos.numero
                         resultado += pos.numero
                     }else {
-                        resultado += (posMas1.numero - pos.numero)
+                        if(pos.romano == 'I'){
+                            if(posMas1.romano == 'L' || posMas1.romano == 'C' || posMas1.romano == 'D' || posMas1.romano == 'M'){
+                                return 'No es romano - valida I'
+                            }
+                        }
+                        if(pos.romano == 'X'){
+                            if(posMas1.romano == 'D' || posMas1.romano == 'M'){
+                                return 'No es romano - valida X'
+                            }
+                        }
+                        numeroMenor = posMas1.numero - pos.numero
+                        resultado += numeroMenor
                         i++
                     }
                 }
             }
-            //console.log(`Encontrado el valor: ${pos.numero}`)
-        }else{
+        }else{            
             return 'No es romano'          
         }
     }
     return resultado
 }
 
+//Esta función busca el valor entregado en un array que sólo contiene los caracteres romanos válidos
+//retorna undefined si el valor no corresponde a un número romano
 function validarRomano(valor){
     let tabla = [
         {numero: 1, romano: 'I'},
@@ -74,9 +102,11 @@ let resultado = []
 
 for (let i = 0; i < data.length; i++) {
     if(parseInt(data[i])){
+        //Si es número entonces convierte número en romano
         resultado[i] = [data[i],covierteNumero(data[i])]
     } else{
-        resultado[i] = [data[i],convierteRomano(data[i])]
+        //Si no es número entonces envía la cadena en mayúsculas para validar y convertir en número si corresponde
+        resultado[i] = [data[i],convierteRomano(data[i].toUpperCase())]
     }
   }  
 
